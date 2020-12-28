9e7827008cf9e92918a2e1a14a537444e97484f5
sumips <47177346+sumips@users.noreply.github.com>
2020-12-28 16:33:07 +0530

    Add files via upload

---
+++ elif.py
@@ -0,0 +1,10 @@
+a=str(input("enter the value of a"))
+if a=="apple":
+   print("fruit name is apple")
+elif a=="banana":
+   print("fruit name is banana")
+elif a=="orange":
+   print("fruit name is orange")
+else:
+   print("none of the above")
+   
